6ed2cdb138c984e5a6bdfe8e35d90d737a8cbc45
Filip Forsby <forsby@kth.se>
2016-03-11 19:27:25 +0100

    readbit stub

--- rf.c
+++ rf.c
@@ -13,7 +13,7 @@
 
 #define T 250
 
-#define MARGIN 50
+#define M 50
 
 int readPacket()
 {
@@ -25,17 +25,32 @@ int readBit()
 {
     double time = 0, oldtime = 0;
     struct timeval  tv;
+    
     // Read T us H
+    while (readVal());
     
     // Read x us L
+    gettimeofday(&tv, NULL); 
+    oldtime = (tv.tv_sec * 1000000) + tv.tv_usec;
     
+    while (!readVal());
+    
+    gettimeofday(&tv, NULL); 
+    time = (tv.tv_sec * 1000000) + tv.tv_usec;
+    
+    switch(oldtime - time){
+        
     // x = 1 -> '1'
-    
+        case T-M ... T+M: return 1;
     // x = 5 -> '0'
     
     // x = 10 -> 'SYNC'
     
     // x = 40 -> 'PAUSE'
+    
+        default return -1;
+    
+    }
     return 1;
 }
 
